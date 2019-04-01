74486f42887201eb8a2a8caedacf2731cfce373a
Holger Schulz <holger.schulz@durham.ac.uk>
2019-03-31 19:14:55 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
   name = 'leptomts',
-  version = '0.0.2',
+  version = '0.1.0',
   description = 'leptMTS',
   url = 'https://github.com/iamholger/leptomts',
   author = 'Kris Moffat, Jessica Turner, Holger Schulz',
