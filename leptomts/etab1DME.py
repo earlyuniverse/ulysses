0e45a522b20e7d4003d49914f1d994de521c057b
Holger Schulz <holger.schulz@durham.ac.uk>
2019-04-18 16:01:04 -0500

    bugfix

--- leptomts/etab1DME.py
+++ leptomts/etab1DME.py
@@ -5,7 +5,7 @@ from odeintw import odeintw
 
 from numba import jit
 @jit
-def fast_RHS(y0, d, w1, n1eq, epstt,epsmm,epsee,epstm,epste,epsme,c1t,c1m,c1e):
+def fast_RHS(y0, d, w1, n1eq, epstt,epsmm,epsee,epstm,epste,epsme,c1t,c1m,c1e, widtht, widthm):
 
     N1      = y0[0]
     Ntt     = y0[1]
@@ -18,9 +18,6 @@ def fast_RHS(y0, d, w1, n1eq, epstt,epsmm,epsee,epstm,epste,epsme,c1t,c1m,c1e):
     c1mc    = np.conjugate(c1m)
     c1ec    = np.conjugate(c1e)
 
-# thermal widths are set to zero such that we are in the "one-flavoured regime"
-    widtht = 485e-10*self.MP/self.M1
-    widthm = 1.7e-10*self.MP/self.M1
 
     #define the different RHSs for each equation
     rhs1 =      -d*(N1-n1eq)
@@ -47,7 +44,10 @@ class EtaB_1DME(leptomts.LeptoCalc):
             self._n1eq    = self.N1Eq(z)
             self._currx=z
 
-        return fast_RHS(y0,self._d, self._w1, self._n1eq,epstt,epsmm,epsee,epstm,epste,epsme,c1t,c1m,c1e)
+        # thermal widths are set to zero such that we are in the "one-flavoured regime"
+        widtht = 485e-10*self.MP/self.M1
+        widthm = 1.7e-10*self.MP/self.M1
+        return fast_RHS(y0,self._d, self._w1, self._n1eq,epstt,epsmm,epsee,epstm,epste,epsme,c1t,c1m,c1e, widtht, widthm)
 
 
     @property
