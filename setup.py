2177e63d2a7a68276ce9fcf9451260833e7783b6
Holger Schulz <holger.schulz@durham.ac.uk>
2019-04-19 16:20:56 -0500

    Bump version, install scan script

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
   name = 'leptomts',
-  version = '0.1.0',
+  version = '0.2.0',
   description = 'leptMTS',
   url = 'https://github.com/iamholger/leptomts',
   author = 'Kris Moffat, Jessica Turner, Holger Schulz',
@@ -15,7 +15,7 @@ setup(
     'numba',
     'pymultinest'
   ],
-  scripts=['bin/lepto-get'],
+  scripts=['bin/lepto-get', 'bin/lepto-scan'],
   extras_require = {
   },
   entry_points = {
