82906dc9e16501445408d37671a88eedd2a015c4
Holger Schulz <holger.schulz@durham.ac.uk>
2019-04-19 15:48:22 -0500

    update testpars to new pnames, fix imports

--- leptomts/__init__.py
+++ leptomts/__init__.py
@@ -8,26 +8,25 @@ from leptomts.etab2DME                 import EtaB_2DME
 from leptomts.etab3DME                 import EtaB_3DME
 from leptomts.etab1BE                  import EtaB_1BE
 from leptomts.etab2BE                  import EtaB_2BE
-from leptomts.etab2resonant            import EtaB_2Resonant
-from leptomts.etab3dsscattering        import EtaB_3DS_Scattering
-from leptomts.etab3dsscatteringooetaur import EtaB_3DS_Scattering_OOEtauR
-from leptomts.etab3dsblanchett         import EtaB_3DS_Blanchett
+from leptomts.etab3DMEscattering       import EtaB_3DME_Scattering
+from leptomts.etab3DMEscatteringRHtaur import EtaB_3DS_Scattering_RHtaur
+
+from leptomts.etab2resonant            import EtaB_2Resonant # Buggy
 
 testpars = {
         'delta'  :270,
-        'a'      :0,
-        'b'      :0,
-        'theta23':48.7,
-        'theta12':33.63,
-        'theta13': 8.52,
+        'a21'      :0,
+        'a31'      :0,
+        't23':48.7,
+        't12':33.63,
+        't13': 8.52,
         'x1'    :45,
         'y1'    :45,
         'x2'    :45,
         'y2'    :45,
         'x3'    :45,
         'y3'    :45,
-        'ordering':0,
-        'm1'     :-0.60206,
+        'm'     :-0.60206,
         'M1'     :11,
         'M2'     :12,
         'M3'     :15
