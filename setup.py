cd97f765f03bb98ca0efb8123e2b3a94d23f4136
Holger Schulz <holger.schulz@durham.ac.uk>
2019-05-26 05:48:58 -0500

    Mark version 0.1 and include matplotlib as ependency as we need it

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
   name = 'ulysses',
-  version = '1.0',
+  version = '0.1',
   description = 'ULYSSES: Universal LeptogeneSiS Equation Solver',
   url = 'https://github.com/iamholger/ulysses',
   author = 'Kris Moffat, Jessica Turner, Holger Schulz',
@@ -12,6 +12,7 @@ setup(
     'numpy',
     'scipy',
     'numba',
+    'matplotlib',
     'pymultinest'
   ],
   scripts=['bin/uls-calc', 'bin/uls-scan', 'bin/uls-nest'],
