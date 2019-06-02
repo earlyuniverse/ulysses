6ec537ccbebb68db60c2a056e6101260ef6e0d67
Holger Schulz <holger.schulz@durham.ac.uk>
2019-06-02 17:27:15 -0500

    Fix import error for readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
+autodoc_mock_imports = ['ulysses']
 # -- Project information -----------------------------------------------------
 
 project = 'ulysses'
