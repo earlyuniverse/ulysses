7e5c3bf7f3ff46b2175b0e19c81fb4e0671b3a4d
Holger Schulz <holger.schulz@durham.ac.uk>
2019-06-02 17:24:13 -0500

    Bump version in sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Kris Moffat, Holger Schulz, Jessica Turner'
 author = 'Kris Moffat, Holger Schulz, Jessica Turner'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
