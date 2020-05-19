41d6840f2b3dbd1736a64a1ef724df6044f7ffef
KPMoffat <49160161+KPMoffat@users.noreply.github.com>
2020-05-19 10:53:18 +0100

    Add files via upload

--- ulysses/etab1BE2F.py
+++ ulysses/etab1BE2F.py
@@ -9,23 +9,20 @@ def fast_RHS(y0, d, w1, n1eq, epstt,epsmm,epsee,epstm,epste,epsme,c1t,c1m,c1e):
 
     N1      = y0[0]
     Ntt     = y0[1]
-    Nmm     = y0[2]
-    Nee     = y0[3]
-    Nme     = y0[4]
+    Nbb     = y0[2]
     c1tc    = np.conjugate(c1t)
     c1mc    = np.conjugate(c1m)
     c1ec    = np.conjugate(c1e)
-
+    epsbb   = epsee+epsmm
+    p1t     = c1t*c1tc
+    p1b     = c1e*c1ec + c1m*c1mc
 
     #define the different RHSs for each equation
     rhs1 =      -d*(N1-n1eq)
-    rhs2 = epstt*d*(N1-n1eq)-0.5*w1*(2*c1t*c1tc*Ntt               )
-    rhs3 = epsmm*d*(N1-n1eq)-0.5*w1*(2*c1m*c1mc*Nmm + c1e*c1mc*Nme + np.conjugate(c1e*c1mc*Nme)                  )
-    rhs4 = epsee*d*(N1-n1eq)-0.5*w1*(2*c1e*c1ec*Nee + c1e*c1mc*Nme + np.conjugate(c1e*c1mc*Nme)                  )
-    rhs5 = epsme*d*(N1-n1eq)-0.5*w1*(  c1m*c1ec*Nee + c1e*c1ec*Nme + c1m*c1ec*Nmm  + c1m*c1mc*Nme)
+    rhs2 = epstt*d*(N1-n1eq)-p1t*w1*Ntt
+    rhs3 = epsbb*d*(N1-n1eq)-p1b*w1*Nbb
 
-
-    return [rhs1, rhs2, rhs3, rhs4, rhs5]
+    return [rhs1, rhs2, rhs3]
 
 class EtaB_1BE2F(ulysses.ULSBase):
     """
@@ -61,13 +58,13 @@ class EtaB_1BE2F(ulysses.ULSBase):
         c1e   =                 self.c1a(0)
 
         k       = np.real(self.k1)
-        y0      = np.array([0+0j,0+0j,0+0j,0+0j,0+0j], dtype=np.complex128)
+        y0      = np.array([0+0j,0+0j,0+0j], dtype=np.complex128)
 
         params  = np.array([epstt,epsmm,epsee,epstm,epste,epsme,c1t,c1m,c1e,k], dtype=np.complex128)
 
         ys, _      = odeintw(self.RHS, y0, self.zs, args = tuple(params), full_output=True)
-        nb      = self.sphalfact*(ys[-1,1]+ys[-1,2]+ys[-1,3])
+        nb      = self.sphalfact*(ys[-1,1]+ys[-1,2])
 
-        self.ys  = np.real(ys[:, [1,2,3]])
+        self.ys  = np.real(ys[:, [1,2]])
 
         return np.real(nb)
