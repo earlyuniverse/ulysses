50d05cb50943fb3133211f2096c78d51d1855a2e
jturner90 <jessicaturner.5390@gmail.com>
2022-10-05 20:38:14 +0100

    amended setup.py and __init__.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 setup(
   name = 'ulysses',
-  version = '1.0.1',
+  version = '1.0.2',
   description = 'ULYSSES: Universal LeptogeneSiS Equation Solver',
   url = 'https://github.com/earlyuniverse/ulysses',
-  author = 'Kris Moffat, Yuber Perez Gonzalez, Alessandro Granelli, Holger Schulz, Jessica Turner',
+  author = 'Alessandro Granelli, Christopher Leslie, Yuber Perez Gonzalez, Brian Shuve,  Holger Schulz, Jessica Turner, Rosie Walker',
   author_email = 'jessicaturner.5390@gmail.com',
   packages = find_packages(),
   include_package_data = True,
@@ -20,7 +20,7 @@ setup(
     # 'mpi4py'
   ],
   python_requires='>3.6.0',
-  scripts=['bin/uls-calc', 'bin/uls-scan', 'bin/uls-nest', 'bin/uls-models'],
+  scripts=['bin/uls-calc', 'bin/uls-scan', 'bin/uls-nest', 'bin/uls-models', 'bin/uls-scan2D'],
   extras_require = {
   },
   entry_points = {
